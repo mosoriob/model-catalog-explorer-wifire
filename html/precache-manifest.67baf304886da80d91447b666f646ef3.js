ce6f599e06846d5f9a037b56c6f7f95f9d83fa1c
Maximilinano Osorio <mosorio@isi.edu>
2022-11-01 13:29:43 -0300

    init

---
+++ html/precache-manifest.67baf304886da80d91447b666f646ef3.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "613f9eb5bea9c2bc902aa660348a8f9b",
+    "url": "images/WIFIRE_LOGO.png"
+  },
+  {
+    "revision": "0d45d23322581f41e4c15678ad1abc5c",
+    "url": "videos/data-1050819440-658.mp4"
+  },
+  {
+    "revision": "d36db8e706f31b311c22206d3a19f978",
+    "url": "videos/data-1050883510-17704.mp4"
+  },
+  {
+    "revision": "3bc1ca3e71415ec2eac8a2221bbd99d2",
+    "url": "images/logo.png"
+  },
+  {
+    "revision": "e867e9a6156a9bcbbdb6",
+    "url": "components/3.js"
+  },
+  {
+    "revision": "3e607910a8c79445698c",
+    "url": "components/5.js"
+  },
+  {
+    "revision": "28c800ef181f4babeb13",
+    "url": "components/6.js"
+  },
+  {
+    "revision": "bd137ff9ffd49618301f",
+    "url": "components/7.js"
+  },
+  {
+    "revision": "27a60a7dc466cdf233d0",
+    "url": "components/app.js"
+  },
+  {
+    "revision": "c45b4abc7a48db2137abb7b904ba6242",
+    "url": "index.html"
+  },
+  {
+    "revision": "694171c663d51ec4482e169c8316cf52",
+    "url": "images/thumbnails/Ethiopia_relief_subbasins_med.png"
+  },
+  {
+    "revision": "b738740fcb3f4a7d9d429a5920f99dc7",
+    "url": "images/favicon.ico"
+  },
+  {
+    "revision": "771c1605d49ec509d5c56efcf4f6898f",
+    "url": "images/manifest/icon-48x48.png"
+  },
+  {
+    "revision": "8e47eb265299282657405d47c8d7ccf6",
+    "url": "images/manifest/icon-72x72.png"
+  },
+  {
+    "revision": "5c25df0296afdb11abbd60f486f29d09",
+    "url": "images/manifest/icon-96x96.png"
+  },
+  {
+    "revision": "9636a8938fc856371d4953c7b4b201a6",
+    "url": "images/mint-icon-32.png"
+  },
+  {
+    "revision": "b82f7d1d6052ab6ab58bd547a63dd618",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "7b06fa97effd37c79d4897db7e4a5636",
+    "url": "images/manifest/icon-144x144.png"
+  },
+  {
+    "revision": "266b89476f0c64d983452c1c545a75e9",
+    "url": "images/manifest/icon-192x192.png"
+  },
+  {
+    "revision": "1286d3109fe9153413fe1031eaf7544b",
+    "url": "images/rdf-disabled.png"
+  },
+  {
+    "revision": "b501cbd04d18d6ca3e4959601f2ff3b0",
+    "url": "images/rdf.png"
+  },
+  {
+    "revision": "2a3cb7cf5a0158e74018a34c546cc637",
+    "url": "images/thumbnails/Blue_Nile_Tribs_relief_and_boundaries_med.png"
+  },
+  {
+    "revision": "34e41b56a48741e7014db2765f1919c0",
+    "url": "images/QUIC-URB.jpg"
+  },
+  {
+    "revision": "e823cad31d1ecd676c4b",
+    "url": "components/4.js"
+  },
+  {
+    "revision": "519243c527a01e7133a306c9e86bd6ea",
+    "url": "images/manifest/icon-512x512.png"
+  },
+  {
+    "revision": "f6faabc6fe064be4918aae6f11c25a20",
+    "url": "images/WUI.png"
+  },
+  {
+    "revision": "2029fd4340a1eb6cb6721c023a58cdf1",
+    "url": "images/thumbnails/Ethiopia_relief_subbasins_small.png"
+  },
+  {
+    "revision": "0009638a55315f435891ddfab83717a2",
+    "url": "images/thumbnails/virtual-gauge-3.png"
+  },
+  {
+    "revision": "2a51c1d98e90e8761a4d5261a6c718b6",
+    "url": "images/thumbnails/virtual-gauge-1.png"
+  },
+  {
+    "revision": "a453e8ddf256fb790e2264dedabc7b94",
+    "url": "images/thumbnails/virtual-gauge-2.png"
+  },
+  {
+    "revision": "067c668beb358cf8f4db93c938bda604",
+    "url": "videos/VirtualGage2.mp4"
+  },
+  {
+    "revision": "9e3151b56e1f2140bf3004962e3e21a5",
+    "url": "videos/VirtualGage3.mp4"
+  },
+  {
+    "revision": "8f338d41124583b92b9f17cc0a8a97b5",
+    "url": "images/QUIC-Fire.png"
+  },
+  {
+    "revision": "2a6b1f72cc981462858ba22367a5783a",
+    "url": "videos/VirtualGage1.mp4"
+  },
+  {
+    "revision": "5175ea65b1ebe0a0a912db1170858939",
+    "url": "images/FastFuels.png"
+  },
+  {
+    "revision": "ac76a2d2607ad7d2f9d00d2b540f720f",
+    "url": "images/thumbnails/GageLocation.png"
+  },
+  {
+    "revision": "baa7879368e28cae3b6dda955f6ac8ce",
+    "url": "images/thumbnails/Blue_Nile_Tribs_relief_and_boundaries_small.png"
+  },
+  {
+    "revision": "f1875732a71cde1ac02a1ff6d03302bd",
+    "url": "images/thumbnails/ethiopia-river-width-visualization.png"
+  },
+  {
+    "revision": "3cc12c6fba440be6fd01de6c069f5de0",
+    "url": "images/example.gif"
+  },
+  {
+    "revision": "a2309a63b1d43e50bf5e",
+    "url": "components/2.js"
+  },
+  {
+    "revision": "37b09a076f5cb20ab5b4",
+    "url": "components/1.js"
+  },
+  {
+    "revision": "50de9394d489da61c5e2",
+    "url": "components/0.js"
+  },
+  {
+    "revision": "b14a827a55c7cfd1495680984b11c671",
+    "url": ".htaccess"
+  }
+];
